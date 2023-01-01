ab1614634a0811c0eb3082a655e3bf7a28a72004
github.28 <narsimha905238368>
2023-01-01 11:57:02 +0530

    first commit

---
+++ Bulb.js
@@ -0,0 +1,11 @@
+function bulbs() {
+    document.getElementById("bulb").src = "images/bulb-on.jpg";
+}
+function bulb2() {
+    document.getElementById("bulb").src = "images/light-bulb-off.jpg";
+}
+let fruit = "Orange";
+marks = 40;
+
+console.log(delete fruit);
+console.log(delete marks);
